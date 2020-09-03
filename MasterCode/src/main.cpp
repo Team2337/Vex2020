f09a80e04fb810ecf9aa78456ffb9b4436d05320
M. Francis <58670911+mfrancis2337@users.noreply.github.com>
2020-09-03 19:45:20 -0400

    Fixed formatting in Master Code
    Fixed formatting in master code by removing unneccessary spaces and tabs.

--- MasterCode/src/main.cpp
+++ MasterCode/src/main.cpp
@@ -81,106 +81,107 @@ void autonomous(void) {
 void usercontrol(void) {
 
 
-   while (true) {
-    // Chassis 
-     //Get the raw sums of the X and Y joystick axes
-        double front_left  = (double)(Controller1.Axis3.position(pct) + Controller1.Axis4.position(pct));
-        double back_left   = (double)(Controller1.Axis3.position(pct) - Controller1.Axis4.position(pct));
-        double front_right = (double)(Controller1.Axis3.position(pct) - Controller1.Axis4.position(pct));
-        double back_right  = (double)(Controller1.Axis3.position(pct) + Controller1.Axis4.position(pct));
-        
-        //Find the largest possible sum of X and Y
-        double max_raw_sum = (double)(abs(Controller1.Axis3.position(pct)) + abs(Controller1.Axis4.position(pct)));
-        
-        //Find the largest joystick value
-        double max_XYstick_value = (double)(std::max(abs(Controller1.Axis3.position(pct)),abs(Controller1.Axis4.position(pct))));
-        
-        //The largest sum will be scaled down to the largest joystick value, and the others will be
-        //scaled by the same amount to preserve directionality
-        if (max_raw_sum != 0) {
-            front_left  = front_left / max_raw_sum * max_XYstick_value;
-            back_left   = back_left / max_raw_sum * max_XYstick_value;
-            front_right = front_right / max_raw_sum * max_XYstick_value;
-            back_right  = back_right / max_raw_sum * max_XYstick_value;
-        }
-        
-        //Now to Consider rotation
-        //Naively add the rotational axis
-        front_left  = front_left  + Controller1.Axis1.position(pct);
-        back_left   = back_left   + Controller1.Axis1.position(pct);
-        front_right = front_right - Controller1.Axis1.position(pct);
-        back_right  = back_right  - Controller1.Axis1.position(pct);
-        
-        //What is the largest sum, or is 100 larger?
-        max_raw_sum = std::max(fabs(front_left),std::max(fabs(back_left),std::max(fabs(front_right),std::max(fabs(back_right),100.0))));
-        
-        //Scale everything down by the factor that makes the largest only 100, if it was over
-        front_left  = front_left  / max_raw_sum * 100.0;
-        back_left   = back_left   / max_raw_sum * 100.0;
-        front_right = front_right / max_raw_sum * 100.0;
-        back_right  = back_right  / max_raw_sum * 100.0;
-        
-        //Write the manipulated values out to the motors
-         LeftFrontMotor.spin(fwd,front_left, velocityUnits::pct);
-          LeftBackMotor.spin(fwd,back_left,  velocityUnits::pct);
-        RightFrontMotor.spin(fwd,front_right,velocityUnits::pct);
-         RightBackMotor.spin(fwd,back_right, velocityUnits::pct);
+  while (true) {
+    // Chassis
+    // Get the raw sums of the X and Y joystick axes
+    double front_left = (double)(Controller1.Axis3.position(pct) + Controller1.Axis4.position(pct));
+    double back_left = (double)(Controller1.Axis3.position(pct) - Controller1.Axis4.position(pct));
+    double front_right = (double)(Controller1.Axis3.position(pct) - Controller1.Axis4.position(pct));
+    double back_right = (double)(Controller1.Axis3.position(pct) + Controller1.Axis4.position(pct));
 
-    // DEPRECATED 
-    /*        
-        LeftBackMotor.spin(directionType::fwd, (Controller1.Axis3.value() + Controller1.Axis4.value())/2, velocityUnits::pct); //(Axis3+Axis4)/2;
-        LeftFrontMotor.spin(directionType::fwd, (Controller1.Axis3.value() + Controller1.Axis4.value())/2, velocityUnits::pct); //(Axis3+Axis4)/2;
-        RightBackMotor.spin(directionType::fwd, (Controller1.Axis3.value() - Controller1.Axis4.value())/2, velocityUnits::pct);//(Axis3-Axis4)/2;      
-        RightFrontMotor.spin(directionType::fwd, (Controller1.Axis3.value() - Controller1.Axis4.value())/2, velocityUnits::pct);//(Axis3-Axis4)/2;
-   */
+    // Find the largest possible sum of X and Y
+    double max_raw_sum = (double)(abs(Controller1.Axis3.position(pct)) + abs(Controller1.Axis4.position(pct)));
+
+    // Find the largest joystick value
+    double max_XYstick_value = (double)(std::max(abs(Controller1.Axis3.position(pct)), abs(Controller1.Axis4.position(pct))));
+
+    // The largest sum will be scaled down to the largest joystick value, and
+    // the others will be scaled by the same amount to preserve directionality
+    if (max_raw_sum != 0) {
+      front_left = front_left / max_raw_sum * max_XYstick_value;
+      back_left = back_left / max_raw_sum * max_XYstick_value;
+      front_right = front_right / max_raw_sum * max_XYstick_value;
+      back_right = back_right / max_raw_sum * max_XYstick_value;
+    }
+
+    // Now to Consider rotation
+    // Naively add the rotational axis
+    front_left = front_left + Controller1.Axis1.position(pct);
+    back_left = back_left + Controller1.Axis1.position(pct);
+    front_right = front_right - Controller1.Axis1.position(pct);
+    back_right = back_right - Controller1.Axis1.position(pct);
+
+    // What is the largest sum, or is 100 larger?
+    max_raw_sum = std::max(fabs(front_left), std::max(fabs(back_left), std::max(fabs(front_right), std::max(fabs(back_right), 100.0))));
+
+    // Scale everything down by the factor that makes the largest only 100, if it was over
+    front_left = front_left / max_raw_sum * 100.0;
+    back_left = back_left / max_raw_sum * 100.0;
+    front_right = front_right / max_raw_sum * 100.0;
+    back_right = back_right / max_raw_sum * 100.0;
+
+    // Write the manipulated values out to the motors
+    LeftFrontMotor.spin(fwd, front_left, velocityUnits::pct);
+    LeftBackMotor.spin(fwd, back_left, velocityUnits::pct);
+    RightFrontMotor.spin(fwd, front_right, velocityUnits::pct);
+    RightBackMotor.spin(fwd, back_right, velocityUnits::pct);
+
+    // DEPRECATED
+    /*
+      LeftBackMotor.spin(directionType::fwd, (Controller1.Axis3.value() +
+      Controller1.Axis4.value())/2, velocityUnits::pct); //(Axis3+Axis4)/2;
+      LeftFrontMotor.spin(directionType::fwd, (Controller1.Axis3.value() +
+      Controller1.Axis4.value())/2, velocityUnits::pct); //(Axis3+Axis4)/2;
+      RightBackMotor.spin(directionType::fwd, (Controller1.Axis3.value() -
+      Controller1.Axis4.value())/2, velocityUnits::pct);//(Axis3-Axis4)/2;
+      RightFrontMotor.spin(directionType::fwd, (Controller1.Axis3.value() -
+      Controller1.Axis4.value())/2, velocityUnits::pct);//(Axis3-Axis4)/2;
+    */
     // Arm
-         int armSpeedPCT = 50;
+    int armSpeedPCT = 50;
 
-         if(Controller1.ButtonUp.pressing()) {
-            LeftArm.spin(directionType::fwd, armSpeedPCT, velocityUnits::pct);
-            RightArm.spin(directionType::fwd, armSpeedPCT, velocityUnits::pct);
-
-        }
-        else if(Controller1.ButtonDown.pressing()) {
-            LeftArm.spin(directionType::rev, armSpeedPCT, velocityUnits::pct);
-            RightArm.spin(directionType::rev, armSpeedPCT, velocityUnits::pct);
-        }
-        else {
-            LeftArm.stop(brakeType::brake);
-            RightArm.stop(brakeType::brake);
-        }
+    if (Controller1.ButtonUp.pressing()) {
+      LeftArm.spin(directionType::fwd, armSpeedPCT, velocityUnits::pct);
+      RightArm.spin(directionType::fwd, armSpeedPCT, velocityUnits::pct);
+    } else if(Controller1.ButtonDown.pressing()) {
+      LeftArm.spin(directionType::rev, armSpeedPCT, velocityUnits::pct);
+      RightArm.spin(directionType::rev, armSpeedPCT, velocityUnits::pct);
+    } else {
+      LeftArm.stop(brakeType::brake);
+      RightArm.stop(brakeType::brake);
+    }
 
     
-    } 
+  } 
   // CURRENTLY NOT BEING USED
 
   // User control code here, inside the loop
 
   // Prevents drifting
-  /*int limiter = 10;
+  /*int threshold = 10;
 
   while (1) {
-    if(abs(Controller1.Axis1.position(percent)) < limiter){
-      if(abs(Controller1.Axis3.position(percent)) > limiter && abs(Controller1.Axis4.position(percent)) < limiter + 1){
+    if(abs(Controller1.Axis1.position(percent)) < threshold){
+      if(abs(Controller1.Axis3.position(percent)) > threshold && abs(Controller1.Axis4.position(percent)) < threshold + 1){
         //Move forwards/backwards
         LeftFrontMotor.setVelocity(Controller1.Axis3.position(percent), percent);
         RightFrontMotor.setVelocity(Controller1.Axis3.position(percent), percent);
         LeftBackMotor.setVelocity(-Controller1.Axis3.position(percent), percent);
         RightBackMotor.setVelocity(-Controller1.Axis3.position(percent), percent);
       }
-      if(abs(Controller1.Axis3.position(percent)) < limiter + 1 && abs(Controller1.Axis4.position(percent)) > limiter){
+      if(abs(Controller1.Axis3.position(percent)) < threshold + 1 && abs(Controller1.Axis4.position(percent)) > threshold){
         //Move left/right
         LeftFrontMotor.setVelocity(Controller1.Axis3.position(percent), percent);
         RightFrontMotor.setVelocity(-Controller1.Axis3.position(percent), percent);
         LeftBackMotor.setVelocity(Controller1.Axis3.position(percent), percent);
         RightBackMotor.setVelocity(-Controller1.Axis3.position(percent), percent);
       }
-      if((Controller1.Axis3.position(percent) > limiter && Controller1.Axis4.position(percent) > limiter) || (Controller1.Axis3.position(percent) < -limiter && Controller1.Axis4.position(percent) < -limiter)){
+      if((Controller1.Axis3.position(percent) > threshold && Controller1.Axis4.position(percent) > threshold) || (Controller1.Axis3.position(percent) < -threshold && Controller1.Axis4.position(percent) < -threshold)){
         //Move diagonally (up and right or down and left)
         LeftFrontMotor.setVelocity(Controller1.Axis3.position(percent), percent);
         LeftBackMotor.setVelocity(Controller1.Axis3.position(percent), percent);
       }
-      if((Controller1.Axis3.position(percent) > limiter && Controller1.Axis4.position(percent) < -limiter) || (Controller1.Axis3.position(percent) < -limiter && Controller1.Axis4.position(percent) > limiter)){
+      if((Controller1.Axis3.position(percent) > threshold && Controller1.Axis4.position(percent) < -threshold) || (Controller1.Axis3.position(percent) < -threshold && Controller1.Axis4.position(percent) > threshold)){
         //Move diagonally (up and left or down and right)
         LeftFrontMotor.setVelocity(Controller1.Axis3.position(percent), percent);
         LeftBackMotor.setVelocity(Controller1.Axis3.position(percent), percent);
@@ -193,7 +194,7 @@ void usercontrol(void) {
     }
     
 
-    wait(limiter, msec); // Sleep the task for a short amount of time to prevent wasted resources.
+    wait(threshold, msec); // Sleep the task for a short amount of time to prevent wasted resources.
     
   }
 
